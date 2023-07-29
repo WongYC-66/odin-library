4b255e937d7b621a5d205904e6656bf5685abb48
WongYC-66 <yengchien@hotmail.com>
2023-07-30 02:30:42 +0800

    done refactor

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // https://www.theodinproject.com/lessons/node-path-javascript-library
-let myLibrary = [];
+
+// https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript 
+// refactor with Class syntax e.g. https://javascript.info/class
 
 // Event
 window.addEventListener('DOMContentLoaded', () => {
-    // initialise
+    
     document.querySelector('#addBookBtn').addEventListener('click', (e) => {
         e.preventDefault()
         document.querySelector('.formDiv').style.visibility = 'visible';
@@ -19,13 +21,13 @@ window.addEventListener('DOMContentLoaded', () => {
         if (!titleInput.value) return clearAllInput();
         if (!authorInput.value) return clearAllInput();
         if (!pagesInput.value) return clearAllInput();
-        let newBook = Book(
-            authorInput.value,
-            titleInput.value,
-            pagesInput.value,
-            isReadInput.checked
-        )
-        addBookToLibrary(newBook)
+        let newBook = new Book({
+            author: authorInput.value,
+            title: titleInput.value,
+            numOfPages: pagesInput.value,
+            isRead : isReadInput.checked,
+        })
+        myLibrary.addBookToLibrary(newBook)
         clearAllInput();
     })
     document.querySelector('#closeBtn').addEventListener('click', (e) => {
@@ -33,104 +35,116 @@ window.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.formDiv').style.visibility = 'hidden';
         clearAllInput();
     })
-
-    initialise();
+    addRandomBook(); // initialise 3 random books into library
+    
+    
 })
-function Book(author, title, numOfPages, isRead) {
-    return {
-        author,
-        title,
-        numOfPages,
-        isRead
+class Book {
+    constructor({ author, title, numOfPages, isRead }) {
+        this.author = author;
+        this.title = title;
+        this.numOfPages = numOfPages;
+        this.isRead = isRead;
     }
 }
-function addBookToLibrary(book) {
-    myLibrary.push(book)
-    displayBook()
+
+class Library {
+    constructor() {
+        this.library = []
+    }
+    displayBook() {
+        let container = document.querySelector('div')
+        // delete previous
+        const divArray = [...container.querySelectorAll('.card')]
+        divArray.forEach(x => x.remove())
+
+        // add book-card
+        this.library.forEach((x, i) => {
+            let card = document.createElement('div')
+            let newNode1 = document.createElement('h3')
+            let newNode2 = document.createElement('p')
+            let newNode3 = document.createElement('p')
+            let newNode4 = document.createElement('div')
+            let newNode4Btn1 = document.createElement('img')
+            let newNode4Btn2 = document.createElement('img')
+            card.classList.add('card')
+            card.classList.add(x.isRead ? 'done-read' : 'not-read')
+            newNode1.textContent = x.title
+            newNode2.textContent = `by : ${x.author}`
+            newNode3.textContent = `${x.numOfPages} pages`
+            // trash / undo / done icon
+            newNode4Btn1.setAttribute('src', './images/trashCan.svg')
+            if (x.isRead) {
+                newNode4Btn2.setAttribute('src', './images/undo.svg')
+                newNode4Btn2.classList.add('to-undo')
+            } else {
+                newNode4Btn2.setAttribute('src', './images/tick.svg')
+                newNode4Btn2.classList.add('to-tick')
+            }
+            // on-click event, trash-icon remove
+            newNode4Btn1.addEventListener('click', e => {
+                let index = e.target.parentNode.parentNode.getAttribute('data-index')
+                this.removeBookfromLibrary(index)
+            })
+            // on-click event, trash-icon remove
+            newNode4Btn2.addEventListener('click', e => {
+                let index = e.target.parentNode.parentNode.getAttribute('data-index')
+                this.toggleIsReadOfBook(index)
+            })
+            newNode4.classList.add('selectionDiv')
+            newNode4.appendChild(newNode4Btn1)
+            newNode4.appendChild(newNode4Btn2)
+            card.appendChild(newNode1)
+            card.appendChild(newNode2)
+            card.appendChild(newNode3)
+            card.appendChild(newNode4)
+            card.setAttribute('data-index', `${i}`)
+            container.appendChild(card)
+        })
+    }
+    addBookToLibrary(book) {
+        this.library.push(book)
+        this.displayBook()
+    }
+    removeBookfromLibrary(index) {
+        this.library.splice(index, 1)
+        this.displayBook()
+    }
+    toggleIsReadOfBook(index) {
+        this.library[index].isRead = !this.library[index].isRead;
+        this.displayBook()
+    }
 }
-function removeBookfromLibrary(index) {
-    myLibrary.splice(index, 1)
-    displayBook()
-}
-function toggleIsReadOfBook(index) {
-    myLibrary[index].isRead = !myLibrary[index].isRead;
-    displayBook()
-}
+
+
 function clearAllInput() {
     document.querySelector("form").reset();
 }
-function displayBook() {
-    let container = document.querySelector('div')
-    // delete previous
-    const divArray = [...container.querySelectorAll('.card')]
-    divArray.forEach(x => x.remove())
 
-    // add book-card
-    myLibrary.forEach((x, i) => {
-        let card = document.createElement('div')
-        let newNode1 = document.createElement('h3')
-        let newNode2 = document.createElement('p')
-        let newNode3 = document.createElement('p')
-        let newNode4 = document.createElement('div')
-        let newNode4Btn1 = document.createElement('img')
-        let newNode4Btn2 = document.createElement('img')
-        card.classList.add('card')
-        card.classList.add(x.isRead ? 'done-read': 'not-read')
-        newNode1.textContent = x.title
-        newNode2.textContent = `by : ${x.author}`
-        newNode3.textContent = `${x.numOfPages} pages`
-        // trash / undo / done icon
-        newNode4Btn1.setAttribute('src', './images/trashCan.svg')
-        if (x.isRead) {
-            newNode4Btn2.setAttribute('src', './images/undo.svg')
-            newNode4Btn2.classList.add('to-undo')
-        } else {
-            newNode4Btn2.setAttribute('src', './images/tick.svg')
-            newNode4Btn2.classList.add('to-tick')
-        }
-        // on-click event, trash-icon remove
-        newNode4Btn1.addEventListener('click', e => {
-            let index = e.target.parentNode.parentNode.getAttribute('data-index')
-            removeBookfromLibrary(index)
-        })
-        // on-click event, trash-icon remove
-        newNode4Btn2.addEventListener('click', e => {
-            let index = e.target.parentNode.parentNode.getAttribute('data-index')
-            toggleIsReadOfBook(index)
-        })
-        newNode4.classList.add('selectionDiv')
-        newNode4.appendChild(newNode4Btn1)
-        newNode4.appendChild(newNode4Btn2)
-        card.appendChild(newNode1)
-        card.appendChild(newNode2)
-        card.appendChild(newNode3)
-        card.appendChild(newNode4)
-        card.setAttribute('data-index', `${i}`)
-        container.appendChild(card)
-    })
-}
-function initialise() {
-    let book1 = {
+function addRandomBook() {
+    let book1 = new Book({
         author: 'Yc-Wong',
         title: 'Javascript Book #1',
         numOfPages: 10,
         isRead: true
-    }
-    let book2 = {
+    })
+    let book2 = new Book({
         author: 'Yc-Wong',
         title: 'Javascript Book #2',
         numOfPages: 20,
         isRead: true
-    }
-    let book3 = {
+    })
+    let book3 = new Book({
         author: 'Yc-Wong',
         title: 'Javascript Book #3',
         numOfPages: 30,
         isRead: false
-    }
+    })
     // Main
-    addBookToLibrary(book1)
-    addBookToLibrary(book2)
-    addBookToLibrary(book3)
-    // displayBook()
+    myLibrary.addBookToLibrary(book1)
+    myLibrary.addBookToLibrary(book2)
+    myLibrary.addBookToLibrary(book3)
 }
+
+
+let myLibrary = new Library()
